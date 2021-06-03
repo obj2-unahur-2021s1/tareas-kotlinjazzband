d5a8dba57abf16c42ff947d8872315d5e7b5e941
jlfarias <jlfarias2009@gmail.com>
2021-06-03 20:41:44 -0300

    Primer avance con las clases(calculo de sueldo y total tarea simple. pasan test

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,53 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+
+abstract class Tarea() {
+    var horasEstimadas = 0
+    var costoInfraestructura = 0
+
+    val empleados = mutableListOf<Empleado>()
+    fun agregarEmpleado(empleado:Empleado) = empleados.add(empleado)
+    fun nominaDeEmpleados() = empleados
+    fun horasNecesarias() = horasEstimadas / empleados.size
+
+    abstract fun costoTotalTarea(): Int
+}
+
+class TareaSimple() :Tarea() {
+
+    fun costoPorSueldosEmpleados():Int = empleados.sumBy { it.sueldo(this) }
+    override fun costoTotalTarea(): Int = costoPorSueldosEmpleados() + costoInfraestructura
+}
+
+class TareaIntegracion : Tarea() {
+    override fun costoTotalTarea(): Int {
+        TODO("costoSubtareas + bono(3% del costoSubtareas)")
+    }
+}
+
+abstract class Trabajador(tarea:Tarea) {
+    var valorHora = 0
+    fun cantidadHoras(tarea:Tarea) {}
+    abstract fun sueldo(tarea:Tarea): Int
+}
+
+class Empleado(tarea: Tarea): Trabajador(tarea) {
+    override fun sueldo(tarea: Tarea): Int = this.valorHora * tarea.horasNecesarias()
+}
+class Encargado(tarea: Tarea): Trabajador(tarea){
+    override fun sueldo(tarea:Tarea): Int = tarea.horasEstimadas * this.valorHora
+}
+
+interface calculo {
+    fun costoTotalTarea()
+    fun horasTrabajadas()
+}
+
+/*
+***costoTareaSimple =  Sumatoria de (horasDeTrabajo * valorHoraEmp) + (valorHoraEncarg * horasEstimadas) + costoInfraestructura
+* horasTrabajoPorEmpleado = Horas estimadas / Cantidad de empleados
+
+***Costo de Tarea de Integración = costoSubtareas + bono(3% del costoSubtareas)
+* Horas de trabajo de cada empleado = planificación (1 x cada 8 de trabajo) + horasSubtareas
+
+*/
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,53 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
+
+  val program = TareaSimple()
+  program.horasEstimadas = 210
+  program.costoInfraestructura= 300000
+
+  val pedro = Empleado(program)
+  pedro.valorHora = 1000
+  val juan = Empleado(program)
+  juan.valorHora = 2000
+  val alberto = Empleado(program)
+  alberto.valorHora = 1500
+
+  val capporale = Encargado(program)
+  capporale.valorHora = 1750
+
+  program.agregarEmpleado(juan)
+  program.agregarEmpleado(pedro)
+  program.agregarEmpleado(alberto)
+
+  describe("Sueldo de Pedro"){
+    it("sueldo de pedro"){
+      pedro.sueldo(program).shouldBe(70000)
+    }
+    it("Sueldo de Juan"){
+      juan.sueldo(program).shouldBe(140000)
+    }
+    it("Sueldo de Alberto"){
+      alberto.sueldo(program).shouldBe(105000)
+    }
+    it("Sueldo de capporale"){
+      capporale.sueldo(program).shouldBe(367500)
+    }
+
+    it("nomina de Empleados"){
+      program.empleados.shouldContainExactly(juan,pedro, alberto)
+    }
+    it("Costo de tarea simple"){
+      // ojo falta incluir encargado
+      program.costoTotalTarea().shouldBe(615000)
+    }
+
+  }
+
   describe("Una tarea") {
   }
 })
